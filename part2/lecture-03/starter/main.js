bebad361181ca0cb5918be206279f251c1cb11af
alex <907785012@qq.com>
2024-04-16 22:25:07 +0800

    Complete 2-3

---
+++ part2/lecture-03/starter/main.js
@@ -0,0 +1,65 @@
+function AppContent() {
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const usernameClass =
+    username.length <= 5 ? "input-error" : "input";
+  const passwordClass =
+    password.length <= 5 ? "input-error" : "input";
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    if (
+      usernameClass === "input-error" ||
+      passwordClass === "input-error"
+    ) {
+      return;
+    }
+
+    console.log("username: ", username);
+    console.log("password: ", password);
+
+    setUsername("");
+    setPassword("");
+  }
+
+  // JSX syntax
+  return (
+    <main>
+      <h2>Login Form</h2>
+      <form
+        onSubmit={handleSubmit}
+        style={{ textAlign: "center" }}
+      >
+        <input
+          className={usernameClass}
+          type="text"
+          value={username}
+          onChange={(event) =>
+            setUsername(event.target.value)
+          }
+        />
+        <br />
+        <input
+          className={passwordClass}
+          type="password"
+          value={password}
+          onChange={(event) =>
+            setPassword(event.target.value)
+          }
+        />
+
+        <br />
+        <button type="submit" className="btn">
+          Login
+        </button>
+      </form>
+    </main>
+  );
+}
+
+const appEl = document.querySelector("#app");
+const root = ReactDOM.createRoot(appEl);
+
+root.render(<AppContent />);
