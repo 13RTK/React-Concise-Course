25e8f686406cc5fcf3866ba67893406b41a06bbf
alex <907785012@qq.com>
2024-04-17 16:47:02 +0800

    Add 2-4 boilerplate

---
+++ part2/lecture-04/starter/src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const usernameClass = username.length <= 5 ? "input-error" : "input";
+  const passwordClass = password.length <= 5 ? "input-error" : "input";
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    if (usernameClass === "input-error" || passwordClass === "input-error") {
+      return;
+    }
+
+    console.log("username: ", username);
+    console.log("password: ", password);
+
+    setUsername("");
+    setPassword("");
+  }
+
+  return (
+    <main>
+      <h2>Login Form</h2>
+      <form onSubmit={handleSubmit} style={{ textAlign: "center" }}>
+        <input
+          className={usernameClass}
+          type="text"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+        />
+        <br />
+        <input
+          className={passwordClass}
+          type="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+
+        <br />
+        <button type="submit" className="btn">
+          Login
+        </button>
+      </form>
+    </main>
+  );
+}
+
+export default App;
