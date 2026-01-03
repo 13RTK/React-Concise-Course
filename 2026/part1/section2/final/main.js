86775c4795fb31597459203da79ce8ffe7a315ec
alex <907785012@qq.com>
2026-01-03 18:02:56 +0800

    complete section1-2

--- 2026/part1/section2/final/main.js
+++ 2026/part1/section2/final/main.js
@@ -1,9 +1,17 @@
 function App() {
+  const [inputText, setInputText] = React.useState("");
+
+  function handleChange(event) {
+    const changedText = event.target.value;
+
+    setInputText(changedText);
+  }
+
   return (
     <>
       <h1>Hello from React</h1>
-      <input type="text" />
-      <p></p>
+      <input type="text" onChange={handleChange} />
+      <p>{inputText}</p>
     </>
   );
 }
