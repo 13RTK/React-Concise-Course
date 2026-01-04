fe2ebd2712420980434fc8956087adafc4cfb955
alex <907785012@qq.com>
2026-01-05 00:02:05 +0800

    add starter of challenge01

---
+++ 2026/part1/challenge-01/final/main.js
@@ -0,0 +1,44 @@
+/*
+  height: 0px,
+  width: 0px,
+  lineHeight: 0px,
+  transform: `rotate(0deg)`,
+*/
+
+function MyApp() {
+  return (
+    <main>
+      <label>
+        Purple
+        <input type="checkbox" />
+      </label>
+
+      <label>
+        text color
+        <select>
+          <option value="" selected>
+            White
+          </option>
+          <option value="text-black">Black</option>
+          <option value="text-orange">Orange</option>
+        </select>
+      </label>
+
+      <label>
+        Circle Size
+        <input type="number" value="150" />
+      </label>
+
+      <label>
+        Circle Rotate
+        <input type="number" value="0" />
+      </label>
+      <div className="circle">Hi!</div>
+    </main>
+  );
+}
+
+const appEl = document.querySelector("#app");
+const root = ReactDOM.createRoot(appEl);
+
+root.render(<MyApp />);
