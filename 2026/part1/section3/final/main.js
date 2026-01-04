6f286b5124b5f58149f03e5dc744495e11dd12a9
alex <907785012@qq.com>
2026-01-04 14:08:52 +0800

    complete section1-3

--- 2026/part1/section3/final/main.js
+++ 2026/part1/section3/final/main.js
@@ -1,18 +1,55 @@
 function App() {
-  const [inputText, setInputText] = React.useState("");
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
 
-  function handleChange(event) {
-    const changedText = event.target.value;
+  function handleSubmit(event) {
+    event.preventDefault();
 
-    setInputText(changedText);
+    alert(`Username: ${username} Password: ${password}`);
+
+    setUsername("");
+    setPassword("");
   }
 
   return (
-    <>
-      <h1>Hello from React</h1>
-      <input type="text" onChange={handleChange} />
-      <p>{inputText}</p>
-    </>
+    <main
+      style={{
+        border: "1px solid black",
+        textAlign: "center",
+      }}
+    >
+      <h2>Login Form</h2>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          style={{
+            margin: "2px",
+          }}
+          type="text"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+        />
+        <br />
+        <input
+          style={{
+            margin: "2px",
+          }}
+          type="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+
+        <br />
+        <button
+          style={{
+            margin: "1rem",
+          }}
+          type="submit"
+        >
+          Login
+        </button>
+      </form>
+    </main>
   );
 }
 
