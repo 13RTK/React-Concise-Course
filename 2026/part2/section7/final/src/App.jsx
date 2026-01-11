20255a510980a8b8415eef2b15acbba85e79fc91
alex <907785012@qq.com>
2026-01-11 13:47:26 +0800

    complete section 2-7(data fetching with useEffect)

--- 2026/part2/section7/final/src/App.jsx
+++ 2026/part2/section7/final/src/App.jsx
@@ -1,10 +1,34 @@
+import { useEffect, useState } from 'react';
+
 function App() {
+  const adviceURL = 'https://api.adviceslip.com/advice';
+
+  const [advice, setAdvice] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function getAdvice() {
+    setIsLoading(true);
+
+    const response = await fetch(adviceURL);
+    const data = await response.json();
+
+    setIsLoading(false);
+    setAdvice(data.slip.advice);
+  }
+
+  useEffect(() => {
+    getAdvice();
+  }, []);
+
   return (
     <main>
       <h1>Advice App</h1>
-      <p></p>
-      <button>Get Advice</button>
+      <p>{isLoading ? 'Loading...' : advice}</p>
+      <button disabled={isLoading} onClick={getAdvice}>
+        Get Advice
+      </button>
     </main>
   );
 }
+
 export default App;
