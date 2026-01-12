d443639ebb3cc31f76c8ccf54c82d7843564a024
alex <907785012@qq.com>
2026-01-12 20:23:03 +0800

    complete section2-10(useEffect cleanup)

--- 2026/part2/section10/final/src/App.jsx
+++ 2026/part2/section10/final/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import useSWR from 'swr';
 
 function App() {
@@ -6,9 +7,22 @@ function App() {
 
   const { data, isLoading, mutate: getAdvice } = useSWR(adviceURL, fetcher);
 
+  const [currentTime, setCurrentTime] = useState(new Date().toLocaleString());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(new Date().toLocaleString());
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
     <main>
       <h1>Advice App</h1>
+      <span>{currentTime}</span>
       <p>{isLoading ? 'Loading...' : data.slip?.advice}</p>
       <button disabled={isLoading} onClick={getAdvice}>
         Get Advice
